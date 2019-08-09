c2d767d375a0e9d6863ccd18a9ba3b306390016f
Milad <meladattef2017@gmail.com>
2019-08-09 22:42:42 +0200

    finished the server side of the sendgrid webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url'); // built in module
 const mongoose = require('mongoose');
 const requireLogin = require('../middleware/requireLogin');
 const requireCredits = require('../middleware/requireCredits');
@@ -6,8 +9,52 @@ const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 module.exports = app => {
-	app.get('/api/surveys/thanks', (req, res) => {
+	app.get('/api/surveys/:surveyId/:choice', (req, res) => {
 		res.send('thank you for voting!');
+	});
+
+	app.post('/api/surveys/webhooks', (req, res) => {
+		const p = new Path('/api/surveys/:surveyId/:choice'); // here we make a matcher
+
+		_chain(req.body)
+			.map(({ email, url }) => {
+				const pathname = new URL(url).pathname; // to just get the path name out of the url
+				const match = p.test(pathname); // if the pathname contains the surveyId and the choice, match will be an object with both properties, and if one of them doesn't exist match will be null
+				if (match) {
+					return { ...match, email };
+				}
+			})
+			.compact()
+			.uniqBy('email', 'surveyId')
+			.each(({ email, surveyId, choice }) => {
+				//first we find the survey by the id
+				// and inside that survey we find the email with responded no
+				// then in the second object we update
+				// we increase the choice (yes or no) by one
+				// and then we make responded true
+				// we set the date
+				// the last step we exexute the query by (exec)
+				Survey.updateOne(
+					{
+						_id: surveyId,
+						recipients: {
+							$elemMatch: { email, responded: false }
+						}
+					},
+					{
+						$inc: { [choice]: 1 },
+						$set: { 'recipients.$.responded': true },
+						lastResponded: new Date()
+					}
+				).exec();
+			})
+			.value();
+
+		//compact removes any undefined elements in the array
+		// uniqBy func will remove any duplicate elements (objects) that has the same two properties, surveyId and email
+
+		console.log(events);
+		res.send({});
 	});
 
 	// create survey
